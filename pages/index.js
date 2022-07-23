2bd8887f89ce04656e14b5d5e6ab753a0e19c77a
noormohammad20 <mdsaim204@gmail.com>
2022-07-23 23:25:26 +0600

    create dynamic routing on app

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,14 @@ export default function Home() {
         <meta name='keyword' content='nextJS,huntingCoderBlog,huntingCoder'></meta>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <nav className={styles.mainNav}>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
@@ -21,49 +29,30 @@ export default function Home() {
           A Blog For Hunting Coder by a Hunting Coder!
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="blogs">
+          <h2>Popular Blogs</h2>
+          <div className="blogItem">
+            <h3>How to learn javascript in 2022?</h3>
+            <p>javascript is the language used to design logic for the web</p>
+          </div>
         </div>
+        <div className="blogs">
+          <div className="blogItem">
+            <h3>How to learn javascript in 2022?</h3>
+            <p>javascript is the language used to design logic for the web</p>
+          </div>
+        </div>
+        <div className="blogs">
+          <div className="blogItem">
+            <h3>How to learn javascript in 2022?</h3>
+            <p>javascript is the language used to design logic for the web</p>
+          </div>
+        </div>
+
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+
     </div>
   )
 }
