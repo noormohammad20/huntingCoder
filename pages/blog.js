aad06394f341114523e923913e23460b28c866dd
noormohammad20 <mdsaim204@gmail.com>
2022-07-26 13:35:17 +0600

    use server side props method for server side rendaring

--- pages/blog.js
+++ pages/blog.js
@@ -1,33 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/Blog.module.css'
 import Link from 'next/link'
 
-const Blog = () => {
+const Blog = (props) => {
+    const [blogs, setBlogs] = useState(props.allBlogs)
+
     return (
         <div className={styles.container}>
             <main className={styles.main}>
-                <div className="blogs">
-                    <div className={styles.blogItem}>
-                        <Link href={'/blogpost/learn-javascript'}>
-                            <h3>How to learn javascript in 2022?</h3></Link>
-                        <p>javascript is the language used to design logic for the web</p>
+                {blogs.map((blog, index) => {
+                    return <div key={index} className={styles.blogItem}>
+                        <Link href={`/blogpost/${blog.slug}`}>
+                            <h3>{blog.title}</h3></Link>
+                        <p>{blog.content.substr(0, 140)}....</p>
                     </div>
-                </div>
-                <div className="blogs">
-                    <div className={styles.blogItem}>
-                        <h3>How to learn javascript in 2022?</h3>
-                        <p>javascript is the language used to design logic for the web</p>
-                    </div>
-                </div>
-                <div className="blogs">
-                    <div className={styles.blogItem}>
-                        <h3>How to learn javascript in 2022?</h3>
-                        <p>javascript is the language used to design logic for the web</p>
-                    </div>
-                </div>
+                })}
             </main>
         </div>
     )
 }
+export async function getServerSideProps(context) {
+    let data = await fetch('http://localhost:3000/api/blogs')
+    let allBlogs = await data.json()
 
+    return {
+        props: { allBlogs },
+    }
+}
 export default Blog
--- pages/blogPost/[slug].js
+++ pages/blogPost/[slug].js
@@ -1,18 +1,25 @@
-import React from 'react'
-import { useRouter } from 'next/router'
+import React, { useState } from 'react'
 import styles from '../../styles/BlogPost.module.css'
-const Slug = () => {
-    const router = useRouter()
-    const { slug } = router.query
+
+const Slug = (props) => {
+    const [blog, setBlog] = useState(props.myBlog)
     return (
         <div className={styles.container}>
             <main className={styles.main}>
-                <h2>Title of The Page {slug}</h2>
+                <h2>Title of The Page {blog.title}</h2>
                 <hr />
-                <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt beatae perferendis incidunt modi veritatis mollitia voluptates laudantium accusamus quae saepe.</div>
+                <div>{blog.content}</div>
             </main>
         </div>
     )
+}
+export async function getServerSideProps(context) {
+    const { slug } = context.query
+    let data = await fetch(`http://localhost:3000/api/getBlog?slug=${slug}`)
+    let myBlog = await data.json()
+    return {
+        props: { myBlog },
+    }
 }
 
 export default Slug
